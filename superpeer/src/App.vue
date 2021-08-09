bdc3fccf42a152b5206c1c3f06ed90a2a755297f
KamAkbar <kamoldinovakbar1@gmail.com>
2021-08-09 15:19:47 +0500

    fixed

--- superpeer/src/App.vue
+++ superpeer/src/App.vue
@@ -35,7 +35,7 @@ export default {
 }
 </script>
 
-<style lang='scss'>
+<style lang="scss">
 
 @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap');
   .v-btn{
